b544752b1521b2df2beb357ee66d0403d93db99a
Philippe Santoro <phsantoro95@gmail.com>
2022-04-27 20:26:19 -0300

    Header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import './Header.css';
+import SearchBar from './SearchBar';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showSearchBar: false,
+    };
+  }
+
+  handleSearchBar = () => {
+    this.setState((prevState) => ({
+      showSearchBar: !prevState.showSearchBar,
+    }));
+  };
+
   render() {
     const { titlePage } = this.props;
+    const { showSearchBar } = this.state;
 
     return (
-      <nav className="boxHeader">
-        <img
-          src={ profileIcon }
-          alt="Ícone que redireciona para a página de perfil."
-          data-testid="profile-top-btn"
-        />
-        <p
-          data-testid="page-title"
-        >
-          { titlePage }
-        </p>
-        <img
-          src={ searchIcon }
-          alt="Ícone que habilita a pesquisa na página."
-          data-testid="search-top-btn"
-        />
-      </nav>
+      <>
+        <nav className="boxHeader">
+          <Link to="/profile">
+            <img
+              alt="Ícone que redireciona para a página de perfil."
+              src={ profileIcon }
+              data-testid="profile-top-btn"
+            />
+          </Link>
+          <p data-testid="page-title">{titlePage}</p>
+          <input
+            type="image"
+            src={ searchIcon }
+            alt="Ícone que habilita a pesquisa na página."
+            data-testid="search-top-btn"
+            onClick={ this.handleSearchBar }
+          />
+        </nav>
+        <div className="boxSearch">{showSearchBar ? <SearchBar /> : null}</div>
+      </>
     );
   }
 }
--- src/components/HeaderNoSearch.jsx
+++ src/components/HeaderNoSearch.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import './Header.css';
 
@@ -9,16 +10,14 @@ class HeaderNoSearch extends React.Component {
 
     return (
       <nav className="HeaderNoSearch">
-        <img
-          src={ profileIcon }
-          alt="Ícone que redireciona para a página de perfil."
-          data-testid="profile-top-btn"
-        />
-        <p
-          data-testid="page-title"
-        >
-          { titlePage }
-        </p>
+        <Link to="/profile">
+          <img
+            src={ profileIcon }
+            alt="Ícone que redireciona para a página de perfil."
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <p data-testid="page-title">{titlePage}</p>
       </nav>
     );
   }
